32bf0d4f182b4b5bc5e7cb7b808d065e15982be8
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-10-16 13:36:07 +0900

    プレイヤーと木仮完成

---
+++ Assets/Adachi/Scripts/Manager/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーンを読み込みたい時に必要なScript
+/// </summary>
+public class SceneLoader : SingletonMonoBehaviour<SceneLoader>
+{
+    /// <summary>
+    /// Sceneを読み込む関数
+    /// </summary>
+    /// <param name="name">Sceneの名前</param>
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadSceneAsync(name);
+    }
+
+    /// <summary>
+    /// Sceneを読み込む関数
+    /// </summary>
+    /// <param name="name">Sceneの種類(enum)</param>
+    public void LoadScene(SceneType type)
+    {
+        SceneManager.LoadSceneAsync(type.ToString());
+    }
+}
